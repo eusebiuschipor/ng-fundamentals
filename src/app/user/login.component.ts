3acdfb6a85ee3e89b27ef3038c0f5c59a847eb9f
eusebiuschipor <eusebiu@eusebiuschipor.com>
2020-04-14 21:06:11 +0300

    Cap 7

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -3,11 +3,15 @@ import { AuthService } from './auth.service'
 import { Router } from '@angular/router'
 
 @Component({
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styles: [`
+    em { float: right, color: #E05C65; padding-left: 10px; }
+  `]
 })
 export class LoginComponent {
-  userName:any;
-  password:any;
+  userName: any;
+  password: any;
+  mouseoverLogin: any;
 
   constructor(private authService: AuthService, private router: Router) {
 
